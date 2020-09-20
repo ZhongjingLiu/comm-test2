af98bc13a41238b74357713d484ec0790b773a43
ZhongjingLiu <38767109+ZhongjingLiu@users.noreply.github.com>
2020-09-20 15:39:23 -0400

    Update comment-handler.js

--- src/functions/comment-handler.js
+++ src/functions/comment-handler.js
@@ -5,7 +5,7 @@ var request = require("request");
 // populate environment variables locally.
 require('dotenv').config()
 
-const URL = "https://jamstack-comments.netlify.com";
+const URL = "https://whocomments2.netlify.app";
 
 /*
   Our serverless function handler
